a4f11b986610e0ed0e15f096504e9104ab1a4e30
Nishant Niroula <niroulanishant910@gmail.com>
2025-11-10 17:56:54 +0545

    added login basic tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:day1/welcome.dart';
+import 'package:day1/view/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "flutter practice", home: Welcome());
+    return MaterialApp(title: "flutter practice", home: Login());
   }
 }
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 
-class Welcome extends StatelessWidget {
-  const Welcome({super.key});
+class Login extends StatelessWidget {
+  const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        child: Container(
-          color: Colors.amber,
-          width: double.maxFinite,
-
-          height: double.maxFinite,
-          child: Container(
-            margin: EdgeInsets.all(30),
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.center,
-            color: Colors.lightBlueAccent,
-            child: Image.asset("assets/icon.png"),
+      appBar: AppBar(title: Text("Login")),
+      body: ListView(
+        children: [
+          Text(
+            "welcome!",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
           ),
-        ),
+          Text(
+            "sign in to continue",
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+          ),
+        ],
       ),
     );
   }

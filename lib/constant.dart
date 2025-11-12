a4d07b09de2a4395399fa6e8fd8c80d3cc2211d2
Nishant Niroula <niroulanishant910@gmail.com>
2025-11-12 21:27:20 +0545

    added svg and styling login page

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 // ignore: constant_identifier_names
-const PRIMARY_COLOR = Colors.blue;
+const PRIMARY_COLOR = Color.fromARGB(255, 4, 15, 139);
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -8,63 +8,118 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //appBar: AppBar(title: Text("Login")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 50,
+              //width: 30,
+              //padding: EdgeInsets.only(right: 200),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back, size: 30),
 
-        children: [
-          Padding(padding: EdgeInsets.only(top: 50)),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.arrow_back),
-            padding: EdgeInsets.only(right: 100),
-          ),
-
-          Text(
-            "welcome!",
-            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
-          ),
-          Text(
-            "sign in to continue",
-            style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-          ),
-          SizedBox(height: 20),
-          Form(
-            child: Column(
-              children: [
-                TextFormField(decoration: InputDecoration(hint: Text("Email"))),
-                SizedBox(height: 20),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(hint: Text("password")),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: PRIMARY_COLOR,
-                  ),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontFamily: "times new roman"),
-                  ),
-                ),
-                TextButton(onPressed: () {}, child: Text("Forgot Password?")),
-                Text("social media login"),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.facebook),
-                    SizedBox(width: 5),
-                    Icon(Icons.mail),
-                    SizedBox(width: 5),
-                    Icon(Icons.apple),
-                  ],
-                ),
-              ],
+                padding: EdgeInsets.only(right: 400),
+              ),
             ),
-          ),
-        ],
+
+            Text(
+              "welcome!",
+              style: TextStyle(fontSize: 44, fontWeight: FontWeight.w800),
+            ),
+
+            Text(
+              "sign in to continue",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(height: 50),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Email",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  TextFormField(
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(prefixIcon: Icon(Icons.email)),
+                  ),
+                  SizedBox(height: 50),
+                  Text(
+                    "Password",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  TextFormField(
+                    style: TextStyle(fontSize: 20),
+                    obscureText: true,
+                    decoration: InputDecoration(prefixIcon: Icon(Icons.lock)),
+                  ),
+
+                  SizedBox(height: 40),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Center(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: PRIMARY_COLOR,
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 45),
+
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        child: Text(
+                          "L O G I N",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "___________"
+                    "or"
+                    "___________",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    "social media login",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.facebook),
+                      SizedBox(width: 5),
+                      Icon(Icons.mail),
+                      SizedBox(width: 5),
+                      Icon(Icons.apple),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

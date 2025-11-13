4f6abae62ad5b0251055a1eb008ab65935c0cee7
Nishant Niroula <niroulanishant910@gmail.com>
2025-11-13 16:02:17 +0545

    image problem

--- lib/view/pages/onboarding/first_onboarding.dart
+++ lib/view/pages/onboarding/first_onboarding.dart
@@ -7,8 +7,20 @@ class FirstOnboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
+        mainAxisSize: MainAxisSize.min,
+
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Image.asset("assets/onboarding/onboarding_image.png")],
+        children: [
+          Container(
+            //padding: EdgeInsets.fromLTRB(50, 200, 50, 200),
+            child: Image.asset(
+              "assets/onboarding/onboarding_image.png",
+              //fit: BoxFit.cover,
+              height: 50,
+              width: 50,
+            ),
+          ),
+        ],
       ),
     );
   }

ad18ae8cca09b8407ed431659f96b469ff84be2c
Nishant Niroula <niroulanishant910@gmail.com>
2025-11-13 17:53:54 +0545

    added navigation to loginpage from onboarding

--- lib/view/pages/onboarding/first_onboarding.dart
+++ lib/view/pages/onboarding/first_onboarding.dart
@@ -1,3 +1,5 @@
+import 'package:day1/constant.dart';
+import 'package:day1/view/pages/auth/login.dart';
 import 'package:flutter/material.dart';
 
 class FirstOnboarding extends StatelessWidget {
@@ -7,9 +9,49 @@ class FirstOnboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        padding: const EdgeInsets.fromLTRB(20, 100, 20, 20),
         child: ListView(
-          children: [Image.asset("assets/bording.png", height: 50, width: 50)],
+          children: [
+            Image(image: AssetImage("assets/Home.png")),
+            SizedBox(height: 30),
+            Text(
+              "welcome",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w800),
+            ),
+            Text(
+              "create an account to access app content",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 19, fontWeight: FontWeight.w300),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(double.infinity, 45),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
